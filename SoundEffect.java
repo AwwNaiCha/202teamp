cd99392780ab87e18e6bb9c9cda8e202586fbbde
J Gao <chienshin1@gmail.com>
2015-11-23 16:51:31 -0800

    Create SoundEffect.java

---
+++ SoundEffect.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class SoundEffect here.
+ * 
+ * @author Jianxin
+ * @version (a version number or a date)
+ */
+public interface SoundEffect  
+{
+     void play();
+}
